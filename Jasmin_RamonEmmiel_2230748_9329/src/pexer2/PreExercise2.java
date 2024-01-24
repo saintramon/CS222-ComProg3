feb0969377446d0c6f6353f3b1e8829a41b009db
saintramon <ramonjasmin8@gmail.com>
2024-01-24 19:57:09 +0800

    Finished the exercise 2

--- Jasmin_RamonEmmiel_2230748_9329/src/pexer2/PreExercise2.java
+++ Jasmin_RamonEmmiel_2230748_9329/src/pexer2/PreExercise2.java
@@ -1,6 +1,44 @@
 /**
  * JASMIN, Ramon Emmiel P.
  * 2230748
+ *
+ * SAMPLE RUN (Continuous)
+ *
+ * saintramon@saintramon 2ndsem % telnet localhost 2000
+ * Trying ::1...
+ * Connected to localhost.
+ * Escape character is '^]'.
+ * What is your name?
+ * Ramon
+ * What is your age?
+ * 18
+ * Ramon, you may exercise your right to vote!
+ * Thank you and have a good day!
+ * Connection closed by foreign host.
+ *
+ * saintramon@saintramon 2ndsem % telnet localhost 2000
+ * Trying ::1...
+ * Connected to localhost.
+ * Escape character is '^]'.
+ * What is your name?
+ * Allie
+ * What is your age?
+ * 19
+ * Allie, you may exercise your right to vote!
+ * Thank you and have a good day!
+ * Connection closed by foreign host.
+ *
+ * saintramon@saintramon 2ndsem % telnet localhost 2000
+ * Trying ::1...
+ * Connected to localhost.
+ * Escape character is '^]'.
+ * What is your name?
+ * Juan
+ * What is your age?
+ * 13
+ * Juan, you are still too young to vote!
+ * Thank you and have a good day!
+ * Connection closed by foreign host.
  */
 package pexer2;
 
@@ -18,42 +56,48 @@ public class PreExercise2 {
         int port = 2000;
 
         try(
-                ServerSocket serverSocket = new ServerSocket(port);
-                Socket clientSocket = serverSocket.accept();
-
-                BufferedReader streamRdr = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter streamWtr = new PrintWriter(clientSocket.getOutputStream(), true);
-
+                ServerSocket serverSocket = new ServerSocket(port); //The server socket is in the outer try-with-resources so that it will only close manually
                 ){
 
-            streamWtr.println("What is your name? ");
-
-            String name = streamRdr.readLine();
-            int age;
-
             while (true){
-                streamWtr.println("What is your age? ");
-                try {
-                    age = Integer.parseInt(streamRdr.readLine());
+                Socket clientSocket = serverSocket.accept();
 
-                    if (age <= 0){
-                        throw new NumberFormatException();
-                    }else {
-                        break;
+                try (
+                        //The stream reader and writer is in an inner try with resources so that it will close after every connection
+                        BufferedReader streamRdr = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        PrintWriter streamWtr = new PrintWriter(clientSocket.getOutputStream(),true);
+                        ){
+
+                    streamWtr.println("What is your name? ");
+
+                    String name = streamRdr.readLine();
+                    int age;
+
+                    while (true){
+                        streamWtr.println("What is your age? ");
+                        try {
+                            age = Integer.parseInt(streamRdr.readLine());
+
+                            if (age <= 0){
+                                throw new NumberFormatException();
+                            }else {
+                                break;
+                            }
+                        }catch (NumberFormatException nfe){
+                            streamWtr.println("Please enter a valid age.");
+                            continue;
+                        }
                     }
-                }catch (NumberFormatException nfe){
-                    streamWtr.println("Please enter a valid age.");
-                    continue;
+
+                    if (age >= 18){
+                        streamWtr.println(name + ", you may exercise your right to vote!");
+                    }else {
+                        streamWtr.println(name + ", you are still too young to vote!");
+                    }
+
+                    streamWtr.println("Thank you and have a good day!");
                 }
             }
-
-            if (age >= 18){
-                streamWtr.println(name + ", you may exercise your right to vote!");
-            }else {
-                streamWtr.println(name + ", you are still too young to vote!");
-            }
-
-            streamWtr.println("Thank you and have a good day!");
 
         }catch (IOException ioe){
             ioe.printStackTrace();
